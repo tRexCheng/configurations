56a844b7ff00d189a11c5451f6caac1e4be01001
Rex Cheng <rex.cheng@lionic.com>
2016-08-09 11:44:16 +0800

    Add tmux configuration

---
+++ tmux/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+font_dir="$HOME/.local/share/fonts"
+
+mkdir -p $font_dir
+
+echo "Copying fonts..."
+cp "./fonts/PowerlineSymbols.otf"  "$font_dir/"
+cp "./fonts/10-powerline-symbols.conf" "/etc/fonts/conf.avail/"
+ln -s "/etc/fonts/conf.avail/10-powerline-symbols.conf" "/etc/fonts/conf.d/10-powerline-symbols.conf"
+
+echo "Resetting font cache.."
+fc-cache -f $font_dir
+
+echo "Font installed."
